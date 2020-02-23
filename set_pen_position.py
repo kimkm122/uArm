b5403ea5d44d471349acf9d7d02e8546f3999767
kimkm122 <kimkm122@gmail.com>
2020-02-23 10:02:56 -0800

    Update set_pen_position.py

--- set_pen_position.py
+++ set_pen_position.py
@@ -13,16 +13,16 @@ firmware_version = device_info['firmware_version']
 if firmware_version and not firmware_version.startswith(('0.', '1.', '2.', '3.')):
     swift.set_speed_factor(0.0005)
 swift.set_mode(0)
-speed = 10000
+speed = 20000
 
 #Go to Z=0
 swift.reset(wait=True, z=50, speed=speed)
-swift.set_position(x=100, speed=speed)
+swift.set_position(x=200, speed=speed)
 swift.set_position(y=0)
 swift.set_position(z=0)
 swift.flush_cmd(wait_stop=True)
 
 #Wait for Confirm then Reset
-confirm = input('Enter Y to proceed')
+confirm = input('Enter any key to continue')
 swift.reset(wait=True, z=50, speed=speed)
 swift.disconnect()
