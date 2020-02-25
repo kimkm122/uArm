fce3aae50c667e52cbda61938c426e606fc4c652
kimkm122 <kimkm122@gmail.com>
2020-02-24 21:13:55 -0800

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,5 +1,7 @@
 import pen_activities as pen
 
+#Have a bug that prevents drawing and writing in sequence (port in use).  Only one can be run at a time.
+
 #Example Drawing
 draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
 image = draw.get_edges('samples/cat.png')
@@ -8,5 +10,5 @@ contours = draw.get_contours(image)
 draw.draw_contours(contours, image_scale = scale)
 
 #Example Writing
-write = pen.Writing(start_position = [255, 0])
-write.write_text('Momo', font_file = 'gothgbt.cxf')
+#write = pen.Writing(start_position = [255, 0])
+#write.write_text('Momo', font_file = 'gothgbt.cxf')
