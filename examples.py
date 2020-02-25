073edbd33c7b0e09fe5b3577cbe227727d461685
kimkm122 <kimkm122@gmail.com>
2020-02-24 20:57:16 -0800

    Initial commit

---
+++ examples.py
@@ -0,0 +1,12 @@
+import pen_activities as pen
+
+#Example Drawing
+draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+image = draw.get_edges('samples/cat.png')
+scale = draw.get_scale(image)
+contours = draw.get_contours(image)
+draw.draw_contours(contours, image_scale = scale)
+
+#Example Writing
+write = pen.Writing(start_position = [255, 0])
+write.write_text('Tigger', font_file = 'gothgbt.cxf')
