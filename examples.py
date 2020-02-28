075d1a50bfc596a9a760d74cdb2957d33896cabd
kimkm122 <kimkm122@gmail.com>
2020-02-28 07:20:37 -0800

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,8 +1,6 @@
 import pen_activities as pen
 import cv2
 
-#Have a bug that prevents drawing and writing in sequence (port in use).  Only one can be run at a time.
-
 #Example Drawing
 #draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
 #image = draw.get_edges('samples/cat.png')
@@ -10,10 +8,11 @@ import cv2
 #contours = draw.get_contours(image)
 #draw.draw_contours(contours, image_scale = scale)
 
+#Example Stipple
+draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+stipples = draw.get_numpy_stipples('samples/stipple.npy')
+draw.draw_stipples(stipples)
+
 #Example Writing
 #write = pen.Writing(start_position = [255, 0])
 #write.write_text('Momo', font_file = 'gothgbt.cxf')
-
-#Example Stipple
-draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
-draw.draw_stipples('stipple.npy')
