14ddfeb80358595739b2fd24ba569e808480bb1c
kimkm122 <kimkm122@gmail.com>
2020-03-02 09:50:34 -0800

    Update examples.py

--- examples.py
+++ examples.py
@@ -3,27 +3,27 @@ import cv2
 
 pen.set_pen_position()
 #Draw outline
-#draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+#draw = pen.Drawing(draw_area = [[-110, -10], [150, 250]])
 #draw.draw_border()
 
 #Example Drawing
-#draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+#draw = pen.Drawing(draw_area = [[-110, -10], [150, 250]])
 #image = draw.get_edges('samples/cat.png')
 #scale = draw.get_scale(image)
 #contours = draw.get_contours(image)
 #draw.draw_contours(contours, image_scale = scale)
 
-#draw = draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+#draw = draw = pen.Drawing(draw_area = [[-110, -10], [150, 250]])
 #image = draw.get_edges('samples/cat.png')
 #scale = draw.get_scale(image)
 #contours = draw.get_contours(image)
 #draw.draw_contour_stipples(contours, image_scale = scale)
 
 #Example Stipple
-draw = pen.Drawing(draw_area = [[150, 250], [50, 150]])
+draw = pen.Drawing(draw_area = [[-110, -10], [150, 250]])
 stipples = draw.get_numpy_stipples('samples/20000.npy')
 draw.draw_stipples(stipples)
 
 #Example Writing
-#write = pen.Writing(start_position = [255, 0])
+#write = pen.Writing(start_position = [10, 240])
 #write.write_text('Momo', font_file = 'gothgbt.cxf')
