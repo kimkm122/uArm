2791e5091b86908deb4fa739903ad25ef78cbf90
kimkm122 <kimkm122@gmail.com>
2020-03-01 18:03:36 -0800

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,6 +1,11 @@
 import pen_activities as pen
 import cv2
 
+pen.set_pen_position()
+#Draw outline
+#draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+#draw.draw_border()
+
 #Example Drawing
 #draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
 #image = draw.get_edges('samples/cat.png')
@@ -8,9 +13,15 @@ import cv2
 #contours = draw.get_contours(image)
 #draw.draw_contours(contours, image_scale = scale)
 
+#draw = draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
+#image = draw.get_edges('samples/cat.png')
+#scale = draw.get_scale(image)
+#contours = draw.get_contours(image)
+#draw.draw_contour_stipples(contours, image_scale = scale)
+
 #Example Stipple
-draw = pen.Drawing(draw_area = [[150, 250], [-50, 50]])
-stipples = draw.get_numpy_stipples('samples/stipple.npy')
+draw = pen.Drawing(draw_area = [[150, 250], [50, 150]])
+stipples = draw.get_numpy_stipples('samples/20000.npy')
 draw.draw_stipples(stipples)
 
 #Example Writing
